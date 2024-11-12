06d1fa910b0766086f350762b4868758dc495536
unknown <zhaobangliuagl@gmail.com>
2024-11-12 15:40:10 +0800

    Add more cable

--- include/cyclone/plinks.h
+++ include/cyclone/plinks.h
@@ -60,6 +60,27 @@ namespace cyclone
 
     unsigned ParticleRod::fillContact(ParticleContact* contact, unsigned limit) const
     {
-        real currentLeng = currentLength();
+        real currentLen = currentLength();
+
+        if(currentLen == length) return 0;
+        
+        contact->particle[0] = particle[0];
+        contact->particle[1] = particle[1];
+
+        Vector3 normal = particle[1]->getPosition() - particle[0]->getPosition();
+        normal.normalize();
+
+        if(currentLen > length)
+        {
+            contact->contactNormal = normal;
+            contact->penetration = currentLen - length;
+        }
+        else
+        {
+            contact->contactNormal = normal * -1;
+            contact->penetration = length - currentLen;
+        }
+        contact->restitution = 0;
+        return 1;
     }
 }
